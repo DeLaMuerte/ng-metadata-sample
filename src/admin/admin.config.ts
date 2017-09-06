b560964ac77abf7e533a777aabfa7c27f818397e
GregorSondermeier <gregor.sondermeier.github@gmail.com>
2017-09-06 14:41:12 +0200

    simple permission check in admin routes

--- src/admin/admin.config.ts
+++ src/admin/admin.config.ts
@@ -1,4 +1,6 @@
 import {Uribuilder} from '../_vanilla/Uribuilder';
+import {LoginService} from '../login/login.service';
+import {CommonAlertService} from '../common/alert/common.alert.service';
 
 export function AdminConfig($routeProvider: ng.route.IRouteProvider) {'ngInject';
 
@@ -6,7 +8,16 @@ export function AdminConfig($routeProvider: ng.route.IRouteProvider) {'ngInject'
 		['index',	'/admin']
 	]));
 
-	$routeProvider.when(Uribuilder.Instance.getPath('admin', 'index'), {
-		template: '<gsc-admin></gsc-admin>'
-	});
+	$routeProvider
+		.when(Uribuilder.Instance.getPath('admin', 'index'), {
+			template: '<gsc-admin></gsc-admin>',
+			resolve: {
+				permitted: ($location: ng.ILocationService, commonAlertService: CommonAlertService, loginService: LoginService) => {'ngInject';
+					if (!loginService.userCtxSubject.getValue() || loginService.userCtxSubject.getValue().roles.indexOf('_admin') == -1) {
+						commonAlertService.error('Not permitted');
+						$location.path(Uribuilder.Instance.getPath('home', 'index'));
+					}
+				}
+			}
+		});
 }
