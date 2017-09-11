14342cee537d9b67c81a0a5092d539bb8fda2f18
GregorSondermeier <gregor.sondermeier.github@gmail.com>
2017-09-11 18:44:10 +0200

    admin tabset update

--- src/admin/admin.config.ts
+++ src/admin/admin.config.ts
@@ -4,17 +4,39 @@ import {CommonAlertService} from '../common/alert/common.alert.service';
 
 export function AdminConfig($stateProvider: ng.ui.IStateProvider) {'ngInject';
 
-	$stateProvider.state({
-		name: 'admin',
-		url: '/admin',
-		component: 'gscAdmin',
-		// resolve: {
-		// 	permitted: ($location: ng.ILocationService, commonAlertService: CommonAlertService, loginService: LoginService) => {'ngInject';
-		// 		if (!loginService.userCtxSubject.getValue() || loginService.userCtxSubject.getValue().roles.indexOf('_admin') == -1) {
-		// 			commonAlertService.error('Not permitted');
-		// 			$location.path(Uribuilder.Instance.getPath('home', 'index'));
-		// 		}
-		// 	}
-		// }
-	});
+	$stateProvider
+		.state({
+			name: 'admin',
+			url: '/admin',
+			component: 'gscAdmin',
+			redirectTo: 'admin.tab1'
+			// resolve: {
+			// 	permitted: ($location: ng.ILocationService, commonAlertService: CommonAlertService, loginService: LoginService) => {'ngInject';
+			// 		if (!loginService.userCtxSubject.getValue() || loginService.userCtxSubject.getValue().roles.indexOf('_admin') == -1) {
+			// 			commonAlertService.error('Not permitted');
+			// 			$location.path(Uribuilder.Instance.getPath('home', 'index'));
+			// 		}
+			// 	}
+			// }
+		})
+		.state({
+			name: 'admin.tab1',
+			url: '/tab1',
+			component: 'gscAdminTab1'
+		})
+		.state({
+			name: 'admin.tab2',
+			url: '/tab2',
+			component: 'gscAdminTab2'
+		})
+		.state({
+			name: 'admin.tab3',
+			url: '/tab3',
+			component: 'gscAdminTab3'
+		})
+		.state({
+			name: 'admin.tab4',
+			url: '/tab4',
+			component: 'gscAdminTab4'
+		});
 }
